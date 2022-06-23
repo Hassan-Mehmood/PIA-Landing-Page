714617e656ebd8a6341ad2998f304e7e214c53dc
Hassan-Mehmood <89647174+Hassan-Mehmood@users.noreply.github.com>
2022-06-23 15:34:32 +0500

    working on the carousel

--- Javascript/app.js
+++ Javascript/app.js
@@ -1,36 +1,25 @@
 const swiper = new Swiper(".swiper", {
-  // Optional parameters
-  direction: "horizontal",
-  loop: true,
+  allowSlideNext: true,
+  allowSlidePrev: true,
+  allowTouchMove: true,
 
-  // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-  },
-
-  breakpoints: {
-    "@0.75": {
-      slidesPerView: 2,
-      spaceBetween: 20,
-    },
-    "@1.00": {
-      slidesPerView: 3,
-      spaceBetween: 40,
-    },
-    "@1.50": {
-      slidesPerView: 4,
-      spaceBetween: 50,
-    },
-  },
-
-  // Navigation arrows
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
 
-  // And if we need scrollbar
-  scrollbar: {
-    el: ".swiper-scrollbar",
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+
+    480: {
+      slidesPerView: 1,
+    },
+
+    640: {
+      slidesPerView: 4,
+      centeredSlides: true,
+    },
   },
 });
